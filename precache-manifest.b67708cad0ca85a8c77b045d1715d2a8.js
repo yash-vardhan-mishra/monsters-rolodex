871a64324f4fe0706e6418fe6a49ada4157b082c
yash-sage <you@example.com>
2020-05-13 04:40:55 +0530

    Updates

---
+++ precache-manifest.b67708cad0ca85a8c77b045d1715d2a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82caf27f9dc5bb2752131e627144c7e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "41c8ef7bf94679f8f8a7",
+    "url": "/monsters-rolodex/static/css/main.21c28da9.chunk.css"
+  },
+  {
+    "revision": "ed9f4125c6ddc10185a3",
+    "url": "/monsters-rolodex/static/js/2.a281a20f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a281a20f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c8ef7bf94679f8f8a7",
+    "url": "/monsters-rolodex/static/js/main.bf6ab815.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
